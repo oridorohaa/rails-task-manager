f60afebb0e7be5f7efdbd64a6b8087a23f0b7975
Andriana Oridoroha <oridorohaa@gmail.com>
2022-11-22 16:11:02 -0600

    done , need to finish checkbox error

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,51 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to tasks_path
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
+  end
+
+  def completed
+    @task = Task.find(params[:id])
+    @task.completed ? @task.completed = false : @task.completed = true
+    
+    @task.save
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+
+    redirect_to tasks_path, status: :see_other
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,21 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root to: "tasks#home"
+
+  get "/tasks", to: "tasks#index"
+
+  get "tasks/new", to: "tasks#new"
+
+  get "tasks/:id", to: "tasks#show", as: "task"
+
+  post "tasks", to: "tasks#create"
+
+  get "tasks/:id/edit", to: "tasks#edit"
+
+  patch "tasks/:id", to: "tasks#update"
+
+  delete "tasks/:id", to:"tasks#destroy"
+
+  patch "tasks/complete/:id", to:"tasks#completed"
+
 end
